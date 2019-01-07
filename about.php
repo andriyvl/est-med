32bed1edc50474f363ab45353c45155471f03856
Andriy Vl <viychuk.a@gmail.com>
2019-01-07 18:37:43 +0100

    vvv

---
+++ about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML> 
+<html lang="pl-PL">
+<head>
+    <meta name="keywords"  content="medycyna estetyczna Lublin kontakt, gabinet medycyny estetycznej Lublin kontakt, lekarz medycyny estetycznej Lublin kontakt, medycyna estetyczna cennik, medycyna estetyczna Lublin kontakt, medycyna estetyczna Lublin zabiegi, kosmetologia Lublin kontakt, gabinet kosmetologii Lublin, Makijaż permanentny Lublin kontakt, Makijaż Lublin, medycyna estetyczna Lublin, gabinet Lublin, mezoterapia skóry Lublin kontakt, Likwidacja celulitu kontakt Lublin kontakt, mezoterapia skóry Lublin kontakt, Likwidacja celulitu Lublin kontakt, modelowanie twarzy Lublin kontakt, toksyna botulinowa Lublin, kwas hialuronowy Lublin, Eksfoliacja Lublin, wypełnianie zmarszczek, wypełnianie zmarszczek Lublin, kwas hialuronowy Lublin, Leczenie nadpotliwości Lublin kontakt, nici hialuronowe, nici hialuronowe Lublin, nici PDO Lublin, Rękas" />
+    <title>Kontakt | Medycyna estetyczna, kosmetologia, makiaż permanentny Lublin</title>
+    <?php include 'metadane.html' ?>
+</head>
+<body>
+    <?php $page = 'about'; include 'header.php' ?>
+    <main>
+    <?php include 'main.html' ?>
+    </aside>
+</main>
+    <div class="cushycms article" id="about">
+        <img class="profilephoto" id="profilephotosmall" src="pics/photoxs.jpg" alt="Dr. Anna Rękas">
+        <img class="profilephoto" id="profilephotonormal" src="pics/photo.jpg" alt="Dr. Anna Rękas">
+        <div class="whitebox">
+            <h2>Dr. Anna Rękas</h2>
+            <p>Medycyna estetyczna</p>
+            <p>Tel. 609 800 282</p>
+            <ul><li>Absolwent Wydziału Lekarskiego UM Lublin</li>
+                <li>Doktor Nauk medycznych</li>
+                <li>Absolwent Podyplomowej Szkoły Medycyny Estetycznej Polskiego Towarzystwa Lekarskiego w Warszawie </li>
+            </ul>
+            <ul>Wybrane publikacje naukowe :
+                <li>Rozprawa doktorska:Zastosowanie transformacji izobolograficznej do oceny charakteru interakcji umbelifeonu z klasycznymi lekami przeciwpadaczkowymi w teście maksymalnego wstrząsu elektrycznego u myszy.</li>
+                <li>Praca dyplomowa z medycyny estetycznej; Znieczulenie w medycynie estetycznej - wybrane zagadnienia oraz wstępne badania własne dotyczące porównania skuteczności różnych rodzajów znieczulenia do wybranych zabiegów w medycynie estetycznej.</li>
+            </ul>
+
+            
+        </div>
+        <br>
+        <!--  mapa zakomentowana <div>
+            <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d39977.076496291214!2d22.497210247811676!3d51.22704659114046!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722584080c71e57%3A0xe27c07df1052f963!2sKoralowa+9%2C+20-001+Lublin!5e0!3m2!1sen!2spl!4v1525710869978" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <br> -->
+        <!-- male zdjecie <img class="profilephoto" id="profilephotosmall" src="" alt="">
+        <img duze zdjecie class="profilephoto" id="profilephotonormal" src="" alt=""> -->
+        <div class="whitebox">
+            <h2>Urszula Ejtel-Ostaszewska</h2>
+            <p>Kosmetologia: rzesy, makijaz permanenty</p>
+            <p>Tel: 793 286 888</p>
+        </div>
+        </div>
+<?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ cennik.php
@@ -0,0 +1,118 @@
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+<head>
+    <meta name="keywords"  content="medycyna estetyczna Lublin cennik, gabinet medycyny estetycznej Lublin cennik, lekarz medycyny estetycznej Lublin cennik, medycyna estetyczna cennik, medycyna estetyczna Lublin promocje, kosmetologia Lublin cennik, gabinet kosmetologii Lublin cennik, Makijaż Lublin cennik, gabinet Lublin cennik, mezoterapia skóry Lublin cennik, Likwidacja celulitu Lublin cena, mezoterapia skóry Lublin cena, Likwidacja celulitu Lublin cennik, modelowanie twarzy cena Lublin, toksyna botulinowa  Lublin, kwas hialuronowy cena Lublin, Eksfoliacja Lublin cena, wypełnianie zmarszczek Lublin cennik, wypełnianie zmarszczek Lublin cena, kwas hialuronowy Lublin cennik, Leczenie nadpotliwości Lublin cennik, nici hialuronowe Lublin cennik, nici PDO Lublin, Anna Rękas" />
+    <title>Cennik | Medycyna estetyczna, kosmetologia, makiaż permanentny Lublin </title>
+    <?php include 'metadane.html' ?>
+</head>
+<body>
+    <?php $page = 'cennik'; include 'header.php' ?>
+    <main>
+    <?php include 'main.html' ?>
+    </main>
+    <div class="article">
+        <div class="cushycms" id="cennik"><table>
+	<tbody>
+		<tr class="title">
+			<td>Zakres usług</td>
+			<td>Cennik</td>
+		</tr>
+		<tr>
+			<td>Toksyna botulinowa (Azzalure, Bocouture)</td>
+			<td><strong>Od 500</strong> jedna okolica (oczy - kurze łapki, czoło, lwia zmarszczka, szyja, zmarszczki palacza, zmarszczki poprzeczne nosa) 1000 zł - cała twarz</td>
+		</tr>
+		<tr>
+			<td><strong>Leczenie nadpotliwości toksyną botulinową</strong></td>
+			<td><strong>1500</strong> zł ( ręce, stopy, pachy)</td>
+		</tr>
+		<tr>
+			<td><strong>Wypełnianie zmarszczek - modelowanie twarzy kwasem hialuronowym</strong> (okolice policzk&oacute;w, kość jarzmowa, kość skroniowa, bruzdy nosowo-wargowe, linie marionetki, modelowanie kształtu ust, poprawianie konturu żuchwy, modelowane rąk , hamowanie oznak starzenia, przeciwdziałanie grawitacji.)</td>
+			<td><strong>Od 900</strong> zł za 1ml preparatu</td>
+		</tr>
+		<tr>
+			<td><strong>Modelowanie twarzy za pomocą hydroksyapatytu wapnia (Radiess, Crystalis)</strong></td>
+			<td><strong>1&nbsp;400,00</strong> zł ampułka</td>
+		</tr>
+		<tr>
+			<td><strong>Mezoterapia igłowa głęboka Neauvia, Teosyal Mezo, Stylage Hydrate.</strong>
+			<ul>
+				<li>poprawa koloru sk&oacute;ry</li>
+				<li>stymulacja produkcji kolagenu</li>
+				<li>likwidowanie przebarwień</li>
+				<li>redukcja zmarszczek</li>
+				<li>poprawa nawilżenia sk&oacute;ry, rąk, szyi, dekoltu.</li>
+			</ul>
+			</td>
+			<td><strong>400- 700</strong> zł w zależności od rodzaju użytego preparatu.</td>
+		</tr>
+		<tr>
+			<td><strong>Aqualix</strong> - permanentne rozpuszczanie tkanki tłuszczowej ( okolice podbr&oacute;dka, ramiona, uda, bryczesy, brzuch, plecy)</td>
+			<td><strong>250-300</strong> zł ampułka</td>
+		</tr>
+		<tr>
+			<td><strong>Likwidacja celulitu, poprawa wyglądu sk&oacute;ry ud.</strong></td>
+			<td><strong>350,00</strong> zł</td>
+		</tr>
+		<tr>
+			<td><strong>Eksfoliacja (kwasy medyczne)</strong>
+			<ul>
+				<li>leczenie cery trądzikowej</li>
+				<li>poprawa jakości sk&oacute;ry</li>
+				<li>intensywne złuszczania sk&oacute;ry zmęczonej, narażonej na działania promienie słonecznych</li>
+				<li>usuwanie przebarwień i plam starczych i posłonecznych</li>
+			</ul>
+			</td>
+			<td><strong>200 - 450 zł</strong></td>
+		</tr>
+		<tr>
+			<td><strong>Nici PDO</strong><br />
+			(mezoterapia nićmi mono, podnoszenie opadających brwi, kącik&oacute;w ust, miękki lifting twarzy nićmi haczykowymi)</td>
+			<td><strong>od 1000</strong> zł w zależności od ilości i rodzaju użytych nici</td>
+		</tr>
+		<tr>
+			<td><strong>Osocze bogatopłytkowe</strong></td>
+			<td>od 500 zł za okolice np cała twarz lub szyja, dekolt, dłonie, sk&oacute;ra głowy.</td>
+		</tr>
+		<tr>
+			<td><strong>Zabieg głębokiego nawilżenia i regeneracji twarzy z efektem wypełnienia - volumetrii - łączenie działania kilku preparat&oacute;w w jednym - Profhilo</strong></td>
+			<td><strong>1 500,00</strong> zł</td>
+		</tr>
+		<tr>
+			<td><strong>Kwas hialuronowy z bursztynianem</strong> - Odbudowa sk&oacute;ry z nawilżeniem i pobudzeniem kom&oacute;rki na poziomie DNA.</td>
+			<td><strong>700<strong> zł</strong></strong></td>
+		</tr>
+		<tr>
+			<td><strong>Mezoterapia okolicy czerwienie wargowej (alternatywa do stałego wypełniacza)</strong></td>
+			<td><strong>700,00</strong> zł</td>
+		</tr>
+		<tr>
+			<td><strong>Korekta doliny łez Redensity II</strong></td>
+			<td><strong>1 200,00</strong> zł</td>
+		</tr>
+		<tr>
+			<td><strong>Estetyka okolicy oka</strong> - korekta cieni pod oczami, zabieg redukujący uczucie zmęczenia i &quot;worki&quot; pod oczami</td>
+			<td><strong>od 600</strong> zł</td>
+		</tr>
+		<tr>
+			<td><strong>Likwidacja zmarszczek poprzecznych szyi , dekoltu, okolicy przyusznej</strong> (kwas hialuronowy, mezoterapia)</td>
+			<td><strong>Od 800,00zł za 1 ampułkę preparatu</strong></td>
+		</tr>
+		<tr>
+			<td><strong>Kwasy medyczne</strong> (salicylowy, migdałowy, hibiscusowy, AHA etc.)</td>
+			<td>250 zl</td>
+		</tr>
+		<tr>
+			<td><strong>Peeling Pqage</strong></td>
+			<td>350 zl</td>
+		</tr>
+		<tr>
+			<td colspan="2">Ceny mogą ulec zmianie w zależności od rodzaju i rozległości zabiegu oraz od ilości zużytego materiału.</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+    </div>
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
